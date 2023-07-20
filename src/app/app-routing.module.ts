0ce723c48864715293daebe305c73a3cb5e9eab2
Omci <melloulchi.omar@gmail.com>
2023-07-20 13:57:34 +0200

    angular project inited, basic frontend routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
+import { ListingPageComponent } from './listing-page/listing-page.component';
+import { MyListingPageComponent } from './my-listing-page/my-listing-page.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contact/:id', component: ContactPageComponent },
+  { path: 'listings', component: ListingPageComponent, pathMatch: 'full' },
+  { path: 'listings/:id', component: ListingDetailPageComponent },
+  { path: 'edit-listing/:id', component: EditListingPageComponent },
+  { path: 'my-listings', component: MyListingPageComponent },
+  { path: 'new-listing', component: NewListingPageComponent },
+  { path: '**', redirectTo: '/listings' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListingPageComponent } from './listing-page/listing-page.component';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
+import { MyListingPageComponent } from './my-listing-page/my-listing-page.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { FormsModule } from '@angular/forms';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { ListingDataFormComponent } from './listing-data-form/listing-data-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListingPageComponent,
+    ListingDetailPageComponent,
+    MyListingPageComponent,
+    NewListingPageComponent,
+    EditListingPageComponent,
+    NewListingPageComponent,
+    MyListingPageComponent,
+    ContactPageComponent,
+    ListingDataFormComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
